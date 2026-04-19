04ba26fa489b85ad5f85961c1c52cac9beacb441
Adriano.py <clovisadrianodasilvafilho@gmail.com>
2026-04-19 14:38:04 -0300

    Upload

---
+++ Codigo-da-Transformacao-EAD/Modulo_08/Saudação.py
@@ -0,0 +1,3 @@
+## Função de saudação
+def saudacao(nome):
+    print(f"Olá, {nome}! Seja bem-vindo(a)!")
---
+++ Codigo-da-Transformacao-EAD/Modulo_08/funcao.maior.e.menor.py
@@ -0,0 +1,5 @@
+## Função maior e menor
+def maior_menor(lista):
+    maior = max(lista)
+    menor = min(lista)
+    return maior, menor
