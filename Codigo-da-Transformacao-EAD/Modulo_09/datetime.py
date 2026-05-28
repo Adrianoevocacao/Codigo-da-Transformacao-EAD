3b5aa0cedad2b756a96cdd6242f69ec1cca46a77
Adriano.py <clovisadrianodasilvafilho@gmail.com>
2026-05-28 16:42:22 -0300

    Delete Codigo-da-Transformacao-EAD/Modulo_09/datetime.py

--- Codigo-da-Transformacao-EAD/Modulo_09/datetime.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-agora = datetime.now()
-
-data_formatada = agora.strftime("%d/%m/%Y")
-hora_formatada = agora.strftime("%H:%M:%S")
-
-print("Data atual:", data_formatada)
-print("Hora atual:", hora_formatada)
